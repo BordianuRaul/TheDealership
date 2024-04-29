533953c51b1bd0d6b2f98f2016319cca3967eedb
Bordianu Raul <raul.bordianu@stud.ubbcluj.ro>
2024-04-29 21:18:21 +0300

    Network label update

--- Frontend/src/app/network-status/network-status.component.ts
+++ Frontend/src/app/network-status/network-status.component.ts
@@ -1,17 +1,28 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {CarService} from "../cars/shared/car.service";
+import {NgClass} from "@angular/common";
 
 @Component({
   selector: 'app-network-status',
   standalone: true,
-  imports: [],
+  imports: [
+    NgClass
+  ],
   templateUrl: './network-status.component.html',
   styleUrl: './network-status.component.css'
 })
-export class NetworkStatusComponent {
-
+export class NetworkStatusComponent implements OnInit {
+  isConnected: boolean;
 
   constructor(private carService: CarService) {
+    this.isConnected = false;
   }
 
+  ngOnInit() {
+    this.carService.checkNetworkStatus().subscribe(
+      (isConnected) => {
+        this.isConnected = isConnected;
+      }
+    );
+  }
 }
